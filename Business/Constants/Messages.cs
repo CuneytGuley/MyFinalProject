3f0b0622fc891d2f7287e15cdf710a56a3d9d44e
Cüneyt Güley <cuneyt.guley@outlook.com>
2021-02-25 00:33:14 +0300

    25.02.2021 ders sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime = "Sistem bakımda";
         public static string ProductListed = "Ürünler listelendi";
+        public static string ProductCountOfCategoryError = "Kategori ürün sayısını aştınız";
+        public static string ProductNameAlreadyExists = "Bu isimde başka bir ürün var";
+        public static string CategoryLimitExceeded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor";
     }
 }
